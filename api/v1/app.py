babd8cd09aa6fb4298f8983dfa3de10f809ff92d
bakka22 <abobakereltom@gmail.com>
2024-03-31 00:58:12 +0200

    s

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,6 +10,7 @@ app = Flask(__name__)
 host = getenv('HBNB_API_HOST', '0.0.0.0')
 port = int(getenv('HBNB_API_PORT', '5000'))
 
+app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 
 
@@ -17,6 +18,12 @@ app.register_blueprint(app_views)
 def reset(exeption):
     """ reload objects from storage """
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    ''' handles 404 error and gives json formatted response '''
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == "__main__":
