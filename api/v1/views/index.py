790727eb4a084d8b2b5b9a928b0df8982f789605
bakka22 <abobakereltom@gmail.com>
2024-03-30 21:59:29 +0200

    s

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,8 +3,25 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+from models.place import Place
+from models.user import User
+from models import engine
 
+classes = {"states": State, "cities": City, "amenities": Amenity,
+        "reviews": Review, "places": Place, "users": User}
 
 @app_views.route('/status')
 def index():
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats')
+def count():
+    dic = {}
+    for key, cls in classes.items():
+        dic[key] = engine.count(cls)
+    return jsonify(dic)
+
